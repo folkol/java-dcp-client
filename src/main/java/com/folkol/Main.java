36f2cd7285a4e24b2e60f4e5d698dbc4a0b2dc21
Matte <mattias.johansson@atex.com>
2015-05-07 16:19:10 +0200

    WIP

--- src/main/java/com/folkol/Main.java
+++ src/main/java/com/folkol/Main.java
@@ -1,30 +1,45 @@
 package com.folkol;
 
 import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
-import org.codehaus.jackson.type.TypeReference;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.Invocation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.codehaus.jackson.map.DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 public class Main {
     static class Conf {
         static class Node {
+            public Map<String, Integer> ports;
             public String hostname;
         }
 
         public List<Node> nodes;
     }
+
     static class Bucket {
+        static class ServerMap {
+            public String hashAlgorithm;
+            public int numReplicas;
+            public List<String> serverList;
+            public List<List<Integer>> vBucketMap;
+        }
 
+        public String name, saslPassword;
+        public ServerMap vBucketServerMap;
+
+        @Override
+        public String toString() {
+            return String.format("%s %s %s", name, saslPassword, vBucketServerMap.vBucketMap);
+        }
     }
-    static class BucketList extends ArrayList<Bucket> {
 
+    static class BucketList extends ArrayList<Bucket> {
     }
 
     public static void main(String[] args) {
@@ -32,13 +47,16 @@ public class Main {
                 .register(new JacksonJaxbJsonProvider().configure(FAIL_ON_UNKNOWN_PROPERTIES, false))
                 .register(HttpAuthenticationFeature.basic("admin", "password"));
 
-        WebTarget pools = client.target("http://localhost:8091/pools/default");
-        System.out.println(pools.request().get(String.class));
-        Conf conf = pools.request().get(Conf.class);
+        Invocation.Builder pools = client.target("http://localhost:8091/pools/default").request();
+        Conf conf = pools.get(Conf.class);
         for (Conf.Node n : conf.nodes)
-            System.out.println(n.hostname);
+            System.out.println(n.ports);
 
-        WebTarget buckets = client.target("http://localhost:8091/pools/default/buckets");
-        System.out.println(buckets.request().get(BucketList.class));
+        Invocation.Builder buckets = client.target("http://localhost:8091/pools/default/buckets").request();
+        System.out.println(buckets.get(String.class));
+        Bucket cmBucket = buckets.get(BucketList.class).get(0);
+        System.out.println(cmBucket.vBucketServerMap.serverList);
+
+//        new Socket(cmBucket.name)
     }
 }
